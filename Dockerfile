aca1735f82eb942c40682dec25f0d83808ce9d61
Rodrigo Vieira <rodrigo.vieira@cetax.com.br>
2019-05-06 19:44:29 -0300

    TODO fix sqlite conn

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,3 @@ FROM python:alpine
 ENV FLASK_ENV=development
 
 RUN pip install flask flask-jsonpify flask-sqlalchemy flask-restful
-
-COPY code /code/
-
-EXPOSE 5000
-
-# ENTRYPOINT ["python", "/code/server.py"]
--- code/server.py
+++ code/server.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
 
-db_connect = create_engine('sqlite:////chinook.db')
+db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
